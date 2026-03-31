deb95d1d649e7493dd1426fe978f566912651ddb
sibelkheirietd-crypto <sibelkheiri_etd@esgen.edu.dz>
2026-03-31 19:09:31 +0100

    Create script.js
    code script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Données montres (prix en DA)
+const montres = [
+    {id:1,nom:"Rolex Submariner",prix:28000,image:"images/montre1.jpg",categorie:"hommes",description:"Étanche 300m"},
+    {id:2,nom:"Omega Seamaster",prix:22000,image:"images/montre2.jpg",categorie:"hommes",description:"Chronomètre certifié"},
